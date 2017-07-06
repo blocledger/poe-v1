d815a9e652699bbfb6708ebed58fb903c026c8d6
ericmvaughn <eric.vaughn@blocledger.com>
2017-07-06 11:02:20 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
 var request = require('superagent').agent();
-var util = require('../util.js');
-var url = 'http://localhost:3000';
+var baseurl = 'http://localhost:3001';
+var url = 'http://localhost:3000/api';
 
 var testTimeout = 15000;  //wait time in milliseconds before failing a test
 
@@ -15,7 +15,7 @@ describe('Add new user test:', function() {
     it('should return success from the POST call', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/addUser')
+      .post(baseurl + '/addUser')
       .send({userName: 'test@blocledger.com', userPw: 'abc', userPwRepeat: 'abc', userType: 'user'})
       .end(function(err, res) {
         if (err) {
@@ -38,7 +38,7 @@ describe('Application REST interface', function() {
   describe('login user', function() {
     it('should login the test user', function(done) {
       request
-      .post(url + '/login')
+      .post(baseurl + '/login')
       .send({'userName': 'test@blocledger.com', 'userPw': 'abc', 'remember': false})
       .end(function(err, res) {
         assert.isNull(err);
@@ -53,48 +53,51 @@ describe('Application REST interface', function() {
     it('should add a document to the blockchain', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/addDoc')
-      .send({hash: hash1, name: 'dummyFile1.txt', owner: 'Alice', hashType: 'sha256'})
+      .post(url + '/Doc')
+      .send({hash: hash1, name: 'dummyFile1.txt', owner: 'Alice'})
       .end(function(err, res) {
       // console.log(err);
       assert.isNull(err);
       // console.log(res.body);
-      assert.isArray(res.body);
-      assert.property(res.body[0], 'state', 'reply should have a state member');
-      assert.isString(res.body[0].state, 'the state should be a string');
-      assert.equal(res.body[0].state, 'fulfilled', 'the state value should be fulfilled');
+      assert.isObject(res.body);
+      assert.property(res.body, 'hash', 'reply should have a hash member');
+      assert.isString(res.body.hash, 'the hash should be a string');
+      assert.equal(res.body.hash, hash1, 'the hash value should equal the input hash');
       done();
       });
     });
     it('should add a second document', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/addDoc')
-      .send({hash: hash2, name: 'dummyFile2.txt', owner: 'Bob', hashType: 'sha256'})
+      .post(url + '/Doc')
+      .send({hash: hash2, name: 'dummyFile2.txt', owner: 'Bob'})
       .end(function(err, res) {
       assert.isNull(err);
       //console.log(res.body);
-      assert.isArray(res.body);
-      assert.property(res.body[0], 'state', 'reply should have a state member');
-      assert.isString(res.body[0].state, 'the state should be a string');
-      assert.equal(res.body[0].state, 'fulfilled', 'the state value should be fulfilled');
+      assert.isObject(res.body);
+      assert.property(res.body, 'name', 'reply should have a filename');
+      assert.isString(res.body.name, 'and the filename should be a string');
+      assert.equal(res.body.name, 'dummyFile2.txt', 'and the filename should be dummyFile2.txt');
       done();
       });
     });
   });
   describe('/listDoc', function(){
-    it('should return a list of users', function(done) {
+    it('should return a list of documents', function(done) {
       this.timeout(testTimeout);
       request
-      .get(url + '/listDoc')
+      .get(url + '/Doc')
       .accept('application/json')
       .end(function(err, res) {
         assert.isNull(err);
         // console.log(res.body);
-        assert.property(res.body, hash1, 'hash1 should be in the list');
-        var doc = JSON.parse(res.body[hash1]);
-        assert.property(doc, 'Owner', 'Document owner should be present');
-        assert.equal(doc.Owner, 'test@blocledger.com', 'the owner should be test@blocledger.com');
+        assert.isArray(res.body, 'GET /Doc should return an array');
+        var doc = res.body.find(function(d) {
+          return d.hash == hash2;
+        });
+        // console.log(doc);
+        assert.property(doc, 'owner', 'Document owner should be present');
+        assert.match(doc.owner, /Bob/, 'and the owner should be Bob');
         done();
       });
     });
@@ -103,29 +106,33 @@ describe('Application REST interface', function() {
     it('should transfer doc from Alice to Bob', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/editDoc')
-      .send({hash: hash1, owner: 'Bob'})
+      .post(url + '/DocTransfer')
+      .send({doc: hash1, newOwner: 'Bob'})
       .end(function(err, res) {
+        // console.log(err);
         assert.isNull(err);
-        //console.log(res.body);
-        assert.isArray(res.body);
-        assert.property(res.body[0], 'state', 'reply should have a state member');
-        assert.isString(res.body[0].state, 'the state should be a string');
-        assert.equal(res.body[0].state, 'fulfilled', 'the state value should be fulfilled');
+        // console.log(res.body);
+        assert.isObject(res.body, 'and /DocTransfer should return an object');
+        assert.property(res.body, 'transactionId', 'reply should have a transactionId');
+        assert.isString(res.body.transactionId, 'and the transactionId should be a string');
+        assert.isAtLeast(res.body.transactionId.length, 10, 'and the transactionId should be greater than 10 characters');
         done();
       });
     });
     it('Bob should be the new owner', function(done) {
       this.timeout(testTimeout);
       request
-      .get(url + '/listDoc')
+      .get(url + '/Doc')
       .end(function(err, res) {
         assert.isNull(err);
         // console.log(res.body);
-        assert.property(res.body, hash1, 'hash1 should be in the list');
-        var doc = JSON.parse(res.body[hash1]);
-        assert.property(doc, 'Owner', 'Document owner should be present');
-        assert.equal(doc.Owner, 'Bob', 'the owner should be Bob');
+        assert.isArray(res.body, 'GET /Doc should return an array');
+        var doc = res.body.find(function(d) {
+          return d.hash == hash1;
+        });
+        // console.log(doc);
+        assert.property(doc, 'owner', 'Document owner should be present');
+        assert.match(doc.owner, /Bob/, 'and the owner should be Bob');
         done();
       });
     });
@@ -135,26 +142,25 @@ describe('Application REST interface', function() {
     it('should be able to verify the document exists', function(done) {
       this.timeout(testTimeout);
       request
-      .get(url + '/verifyDoc/' + hash1)
+      .get(url + '/Doc/' + hash1)
       .end(function(err, res) {
         assert.isNull(err);
         assert.equal(res.status, 200, 'should receive a 200 status');
         // console.log(res.body);
-        assert.property(res.body, 'Hash', 'response should have a Hash');
-        assert.equal(res.body.Hash, hash1, 'the hash should be the same as the input hash');
+        assert.property(res.body, 'hash', 'response should have a Hash');
+        assert.equal(res.body.hash, hash1, 'the hash should be the same as the input hash');
         done();
       });
     });
     it('should report an error for an unknown hash value', function(done) {
       this.timeout(testTimeout);
       request
-      .get(url + '/verifyDoc/' + 'abcd1234')
+      .get(url + '/Doc/' + 'abcd1234')
       .end(function(err, res) {
-        // console.log(res);
-        assert.isNull(err);
-        assert.equal(res.status, 200, 'should receive a 200 status');
-        assert.property(res.body, 'Error', 'response should have a Error field');
-        assert.equal(res.body.Error, 'Document not found', 'should return document not found');
+        // console.log(res.body);
+        assert.isNotNull(err);
+        assert.equal(res.status, 404, 'should receive a 404 status');
+        assert.property(res.body, 'error', 'response should have a Error field');
         done();
       });
     });
@@ -164,42 +170,37 @@ describe('Application REST interface', function() {
     it('should delete a test document', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/delDoc')
-      .send({hash: hash1})
+      .delete(url + '/Doc/' + hash1)
       .end(function(err, res) {
       assert.isNull(err);
-      //console.log(res.body);
-      assert.isArray(res.body);
-      assert.property(res.body[0], 'state', 'reply should have a state member');
-      assert.isString(res.body[0].state, 'the state should be a string');
-      assert.equal(res.body[0].state, 'fulfilled', 'the state value should be fulfilled');
+      // console.log(res);
+      assert.equal(res.status, 204, 'should receive a 204 status');
       done();
       });
     });
     it('should delete second test document', function(done) {
       this.timeout(testTimeout);
       request
-      .post(url + '/delDoc')
-      .send({hash: hash2})
+      .delete(url + '/Doc/' + hash2)
       .end(function(err, res) {
       assert.isNull(err);
-      //console.log(res.body);
-      assert.isArray(res.body);
-      assert.property(res.body[0], 'state', 'reply should have a state member');
-      assert.isString(res.body[0].state, 'the state should be a string');
-      assert.equal(res.body[0].state, 'fulfilled', 'the state value should be fulfilled');
+      // console.log(res.body);
+      assert.equal(res.status, 204, 'should receive a 204 status');
       done();
       });
     });
     it('both test documents should be gone', function(done) {
       this.timeout(testTimeout);
       request
-      .get(url + '/listDoc')
+      .get(url + '/Doc')
       .end(function(err, res) {
         assert.isNull(err);
         //console.log(res.body);
-        assert.notProperty(res.body, hash1, 'hash1 should not be in the list');
-        assert.notProperty(res.body, hash2, 'hash2 should be not in the list');
+        assert.isArray(res.body, 'GET /Doc should return an array');
+        var doc = res.body.find(function(d) {
+          return (d.hash == hash1 || d.hash == hash2);
+        });
+        assert.isUndefined(doc, 'Documents should be deleted');
         done();
       });
     });
